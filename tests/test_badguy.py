e6e7298bd80b4e38a90a5db665b90d6f81d75d48
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-07-27 18:18:21 +0200

    test(move): move forgotten test from rawmaker

--- tests/test_badguy.py
+++ tests/test_badguy.py
@@ -1,16 +1,28 @@
 # =============================================================================
 # C O P Y R I G H T
 # -----------------------------------------------------------------------------
-# Copyright (c) 2019-2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
 # This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import os
+
+import jam
+import pytest
 import utilatest
 
-import pdfinfo
+import tests
 
-run, fail = utilatest.create_cli_runner(pdfinfo)
 
-security = utilatest.register_marker('security')
+@utilatest.nightly
+@pytest.mark.security
+def test_badguy_longpdf(td, mp):
+    """Test that program success on very long, empty pdf file.
+
+    Long files with content are excluded by file size limit.
+    """
+    very_long = os.path.join(td.tmpdir, 'mejabalong.pdf')
+    jam.write_blank_pdf(100000, very_long)
+    tests.run(f'-i {very_long}', mp=mp)
